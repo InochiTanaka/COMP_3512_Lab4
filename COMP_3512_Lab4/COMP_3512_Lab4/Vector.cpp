5bf82134c6fcebd9a290f2eba823fabbe477d10a
InochiTanaka <yunominosencha@gmail.com>
2018-02-08 13:00:17 -0800

    Add comments to tester

--- COMP_3512_Lab4/COMP_3512_Lab4/Vector.cpp
+++ COMP_3512_Lab4/COMP_3512_Lab4/Vector.cpp
@@ -1,20 +1,16 @@
 #include "Vector.hpp"
 
+//Default constructor
 Vector::Vector() : x{ 0 }, y{ 0 }, z{ 0 }
 {
 }
 
+//Destructor
 Vector::~Vector()
 {
 }
 
-void Vector::three_parameter(double mX, double mY, double mZ)
-{
-	x = mX;
-	y = mY;
-	z = mZ;
-}
-
+//Constructor
 Vector::Vector(const Vector & vect)
 {
 	x = vect.x;
@@ -22,36 +18,51 @@ Vector::Vector(const Vector & vect)
 	z = vect.z;
 }
 
+//Set three parameters through asign
+void Vector::three_parameter(double mX, double mY, double mZ)
+{
+	x = mX;
+	y = mY;
+	z = mZ;
+}
+
+//Return value of x
 double Vector::get_X()
 {
 	return x;
 }
 
+//Return value of y
 double Vector::get_Y()
 {
 	return y;
 }
 
+//Return value of z
 double Vector::get_Z()
 {
 	return z;
 }
 
+//Set value of x by assign value
 void Vector::set_X(int mX)
 {
 	x = mX;
 }
 
+//Set value of y by assign value
 void Vector::set_Y(int mY)
 {
 	y = mY;
 }
 
+//Set value of z by assign value
 void Vector::set_Z(int mZ)
 {
 	z = mZ;
 }
 
+//Clearing value of x, y, and z to 0
 void Vector::clear()
 {
 	x = 0;
@@ -59,6 +70,7 @@ void Vector::clear()
 	z = 0;
 }
 
+//Output each vakue of x, y, and z
 ostream & operator<<(ostream & os, const Vector& vect)
 {
 
@@ -70,6 +82,7 @@ ostream & operator<<(ostream & os, const Vector& vect)
 	return os;
 }
 
+//Prefix Increment
 Vector & Vector::operator++()
 {
 	x++;
@@ -78,6 +91,7 @@ Vector & Vector::operator++()
 	return *this;
 }
 
+//Postfix Increment
 Vector Vector::operator++(int)
 {
 	Vector temp(*this);
@@ -85,6 +99,7 @@ Vector Vector::operator++(int)
 	return temp;
 }
 
+//Prefix decrement
 Vector & Vector::operator--()
 {
 	x--;
@@ -93,6 +108,7 @@ Vector & Vector::operator--()
 	return *this;
 }
 
+//Postfix decrement
 Vector Vector::operator--(int)
 {
 	Vector temp(*this);
@@ -100,6 +116,7 @@ Vector Vector::operator--(int)
 	return temp;
 }
 
+//Swap two Victors
 Vector & Vector::operator=(Vector rhs)
 {
 	swap(*this, rhs);
@@ -114,6 +131,7 @@ void swap(Vector & lhs, Vector & rhs)
 	swap(lhs.z, rhs.z);
 }
 
+//Add each value of x, y, and z with another Vector
 Vector & Vector::operator+=(const Vector & vect)
 {
 	x += vect.x;
@@ -122,6 +140,7 @@ Vector & Vector::operator+=(const Vector & vect)
 	return *this;
 }
 
+//reduce each value of x, y, and z by another Vector
 Vector & Vector::operator-=(const Vector & vect)
 {
 	x -= vect.x;
@@ -130,23 +149,27 @@ Vector & Vector::operator-=(const Vector & vect)
 	return *this;
 }
 
+//Add two vectors
 Vector operator+(Vector lhs, const Vector & rhs)
 {
 	lhs += rhs;
 	return lhs;
 }
 
+//Reduce a vector from another one
 Vector operator-(Vector lhs, const Vector & rhs)
 {
 	lhs -= rhs;
 	return lhs;
 }
 
+//Calculate dot product
 double operator*(Vector lhs, const Vector & rhs)
 {
 	return (lhs.x*rhs.x)+ (lhs.y*rhs.y) + (lhs.z*rhs.z);
 }
 
+//multiply each value of x, y, and z with another Vector
 Vector Vector::operator*(double d)
 {
 	x *= d;
